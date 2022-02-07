ffdf75f846a962ec3a4dd52a6512cf26b5211b06
sergii <sergii_vlasiuk@ukr.net>
2022-02-08 01:14:21 +0200

    code review

--- express-server/app.js
+++ express-server/app.js
@@ -6,31 +6,8 @@ const app = express()
 app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes'))
 
-// await startServices()
 startServices()
-// const mongoose = require('mongoose')
 
-// async function startMongo() {
-//   console.log('start mongo client...')
-//   try {
-//     // await mongoose.connect('mongodb://mongoadmin:secret@localhost:27888/?retryWrites=true&w=majority')
-//     // await mongoose.connect('mongodb://mongo:secret@localhost:27017/chat-schema')
-//     await mongoose.connect(config.get('mongoUri'), {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     const db = mongoose.connection
-//     db.on('error', console.error.bind(console, 'connection error:'))
-//     db.once('open', function callback() {
-//       console.log("h")
-//     })
-//     console.log('Mongo client connected succesfully')
-//   } catch (e) {
-//     console.log('Server Error', e.message)
-//     process.exit(1)
-//   }
-// }
-// startMongo()
 
 const PORT = config.get('port') || 5000
 
